98e330ecf06961a70361bb1a0aed3563f527404a
Eve Reichmann <eve.reichmann@gmail.com>
2020-08-10 11:21:54 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+    function addEventTodo(){
+    //find the form
+    //addeventlistener to submit button
+    //prevent default 
+    //scrip data from form
+    //clear form
+    //append to page 
+        const form = document.getElementById('create-task-form')
+        const toDoList = document.getElementById('tasks')
+        form.addEventListener('submit', function(event){
+            event.preventDefault()
+            const formData = event.target[0].value
+            const li = document.createElement('li')
+            li.innerText = formData
+            toDoList.append(li)
+            form.reset() 
+        })
+    
+    }
+    addEventTodo()
 });
+
